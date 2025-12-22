b810f0d8d4dbb9953026d127aa4b230c09d22a75
mortalpues-pixel <mortalpues@gmail.com>
2025-12-22 21:34:29 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: { type: String, required: true, unique: true },
+    prestige: { type: Number, default: 0 },
+    weekly_prestige: { type: Number, default: 0 },
+    last_weekly_reset: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
